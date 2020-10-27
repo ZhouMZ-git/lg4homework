6b3f0e2eef9917e1c9a15757ef99512a31e5881e
ZhouMZ-git <ZhouMZrun@163.com>
2020-10-27 23:03:22 +0800

    pytest练习

--- python_homework/xuzhu.py
+++ python_homework/xuzhu.py
@@ -1,6 +1,3 @@
-
-
-
 # 创建一个虚竹类继承童姥类
 from python_homework.tonglao import TongLao
 
@@ -10,9 +7,10 @@ class XuZhu(TongLao):
     def read(self):
         print("罪过罪过")
 
-#创建一个虚竹对象
+
+# 创建一个虚竹对象
 a = XuZhu(10, 10)
-#调用父类童姥的方法
+# 调用父类童姥的方法
 a.fight_zms(10, 10)
-#调用自己的方法
+# 调用自己的方法
 a.read()
